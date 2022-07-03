8c7280b483305d182b5dd2aa0dfb216f44a9e8f8
iyha-1999 <iyha1999end@gmail.com>
2022-07-04 07:47:03 +0900

    iframeのローディングアニメーションの実装

--- src/components/Creative.js
+++ src/components/Creative.js
@@ -4,6 +4,7 @@ import Icon from "../images/reload.png";
 
 const Creative = ({ creative }) => {
   const [creativeUrl, setCreativeUrl] = useState(creative.url);
+  const [completedIframeLoad, setCompletedIframeLoad] = useState(false);
   const iframeReload = () => {
     setCreativeUrl("");
     setTimeout(() => {
@@ -12,11 +13,22 @@ const Creative = ({ creative }) => {
   };
   return (
     <div className="c-creative">
+      {completedIframeLoad ? (
+        ""
+      ) : (
+        <div className="c-creative-loader">
+          <div className="c-creative-loader-icon"></div>
+        </div>
+      )}
       <iframe
+        id="iframe"
         title="creative"
         width="300"
         height="250"
         src={creativeUrl}
+        onLoad={() => {
+          setCompletedIframeLoad(true);
+        }}
       ></iframe>
       <aside className="c-creative-discription">{creative.discription}</aside>
       <div className="c-creative-reload-icon" onClick={iframeReload}>
